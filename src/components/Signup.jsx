050117328c4f50eaccf74c92144a7374af4782c9
Francisco Berroeta <fsberroeta@gmail.com>
2025-02-12 18:56:16 -0300

    feat: useActionState. implement form state management and error handling in Signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,5 @@
+import { useActionState } from 'react';
+
 import {
   isEmail,
   hasMinLength,
@@ -6,7 +8,7 @@ import {
 } from '../util/validation.js';
 
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(previusFormState, formData) {
     const email = formData.get('email');
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm-password');
@@ -31,7 +33,7 @@ export default function Signup() {
     }
 
     if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
-      errors.push('Pleas provide both your first and last name.');
+      errors.push('Please provide both your first and last name.');
     }
 
     if (!isNotEmpty(role)) {
@@ -45,10 +47,20 @@ export default function Signup() {
     if (acquisitionChannel.length === 0) {
       errors.push('Please select at least one acquisition channel.');
     }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    return { errors: null };
   }
 
+  const [formState, formAction] = useActionState(signupAction, {
+    errors: null,
+  });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -132,6 +144,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && (
+        <ul className="error">
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
